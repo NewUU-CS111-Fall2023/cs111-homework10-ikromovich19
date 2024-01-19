51409dc6c90ec9464bcb860fa5595676852a51dd
Firdavs Ezozkhonov 20222100103 <149464742+ikromovich19@users.noreply.github.com>
2024-01-19 17:02:42 +0500

    Update task_7.cpp

--- src/cpp/task_7.cpp
+++ src/cpp/task_7.cpp
@@ -1,9 +1,20 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Firdavs Ezozxonov
+ * Date: 1/19/2024
+ * Name: hw-10
  */
 
 class Problem7 {
 public:
+    int numRabbits(vector<int>& answers) {
+        int ans = 0;
+        unordered_map<int,int> mp;
+        for(auto &i: answers){
+            mp[i]++;
+        }
+        for(auto &i: mp){
+            ans += ceil((float)i.second/(i.first+1))*(i.first+1);
+        }
+        return ans;
+    }
 };
